6fe578d0ed1df6b10e73b7cc38d148212de3b59b
Ed Preston <epreston@prestonsoft.com>
2014-05-20 20:40:49 -0400

    Refactor MutableExtensionRegistry

--- src/runtime/Classes/MutableExtensionRegistry.m
+++ src/runtime/Classes/MutableExtensionRegistry.m
@@ -1,63 +1,53 @@
-// Protocol Buffers for Objective C
 //
-// Copyright 2010 Booyah Inc.
-// Copyright 2008 Cyrus Najmabadi
+//  MutableExtensionRegistry.m
+//  Protocol Buffers for Objective C
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//  Copyright 2014 Ed Preston
+//  Copyright 2010 Booyah Inc.
+//  Copyright 2008 Cyrus Najmabadi
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 #import "MutableExtensionRegistry.h"
 
 #import "ExtensionField.h"
 
-@interface PBMutableExtensionRegistry()
-@property (strong) NSMutableDictionary* mutableClassMap;
-@end
 
-@implementation PBMutableExtensionRegistry
-
-@synthesize mutableClassMap;
-
-
-- (instancetype) initWithClassMap:(NSMutableDictionary*) mutableClassMap_ {
-  if ((self = [super initWithClassMap:mutableClassMap_])) {
-    self.mutableClassMap = mutableClassMap_;
-  }
-
-  return self;
+@implementation PBMutableExtensionRegistry {
+    NSMutableDictionary* _mutableClassMap;
 }
 
+- (instancetype)initWithClassMap:(NSMutableDictionary*)mutableClassMap {
+    
+    self = [super initWithClassMap:mutableClassMap];
+    if (self == nil) {
+        return nil;
+    }
+    
+    _mutableClassMap = mutableClassMap;
 
-+ (instancetype) registry {
-  return [[PBMutableExtensionRegistry alloc] initWithClassMap:[NSMutableDictionary dictionary]] ;
+    return self;
 }
 
++ (instancetype)registry {
+    return [[PBMutableExtensionRegistry alloc] initWithClassMap:[NSMutableDictionary dictionary]] ;
+}
 
-- (void) addExtension:(id<PBExtensionField>) extension {
-  if (extension == nil) {
-    return;
-  }
-
-  Class extendedClass = [extension extendedClass];
-  id key = [self keyForClass:extendedClass];
-
-  NSMutableDictionary* extensionMap = [classMap objectForKey:key];
-  if (extensionMap == nil) {
-    extensionMap = [NSMutableDictionary dictionary];
-    [mutableClassMap setObject:extensionMap forKey:key];
-  }
-
-  [extensionMap setObject:extension
-                   forKey:[NSNumber numberWithInteger:[extension fieldNumber]]];
+- (void)addExtension:(id<PBExtensionField>)extension {
+    if (extension == nil) {
+        return;
+    }
+    
+    Class extendedClass = [extension extendedClass];
+    id key = [self keyForClass:extendedClass];
+    
+    NSMutableDictionary* extensionMap = [classMap objectForKey:key];
+    if (extensionMap == nil) {
+        extensionMap = [NSMutableDictionary dictionary];
+        [_mutableClassMap setObject:extensionMap forKey:key];
+    }
+    
+    [extensionMap setObject:extension
+                     forKey:[NSNumber numberWithInteger:[extension fieldNumber]]];
 }
 
 
